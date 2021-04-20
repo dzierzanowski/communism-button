10bd5f4af735aa4b0f86dafd08d1853225763986
Waked <waked@incens.io>
2021-04-20 22:56:56 +0200

    Debug

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-var APP_PREFIX = 'CommunismButton_'
-var VERSION = '0.0.1'
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                    // Add URL you want to cache in this list.
-    '/dzierzanowski/',           // If you have separate JS/CSS files,
-    '/dzierzanowski/index.html'  // add path to those files here
+var CACHE_NAME = 'communism-button-v1'
+var urlsToCache = [                    // Add URL you want to cache in this list.
+    '/dzierzanowski/',          // If you have separate JS/CSS files,
+    '/dzierzanowski/index.html' // add path to those files here
 ]
 
 // Respond with cached resources
@@ -27,24 +25,28 @@ self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log('Installing cache: ' + CACHE_NAME)
-            return cache.addAll(URLS)
+            return cache.addAll(urlsToCache)
         })
     )
 })
 
 // Delete outdated caches
 self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(keyList => {
-            var cacheWhitelist = keyList.filter(/*key => key.indexOf(APP_PREFIX)*/ true)
-            cacheWhitelist.push(CACHE_NAME)
 
-            return Promise.all(keyList.map((key, i) => {
-                if (cacheWhitelist.indexOf(key) === -1) {
-                    console.log('Deleting cache: ' + keyList[i] )
-                    return caches.delete(keyList[i])
-                }
-            }))
+    const cacheAllowList = [ CACHE_NAME ]
+
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            // var cacheWhitelist = cacheNames.filter(/*key => key.indexOf(APP_PREFIX)*/ true)
+            // cacheWhitelist.push(CACHE_NAME)
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheAllowList.indexOf(cacheName) === -1) {
+                        console.log('Deleting cache: ', cacheName)
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
         })
     )
 })
