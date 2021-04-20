40ddbf6c33baf95ab0251931a625f4206f7c6e7b
Waked <waked@incens.io>
2021-04-20 22:18:24 +0200

    Add PWA stub

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Tired of "communism button" apps playing a 30 second ad after just 5 seconds of 
 What sort of communism is it, which puts capitalism first?! Here's a true communism button
 app, where you can get as much communism as you like, distraction-free.
 
+## Acknowledgements
+Thanks to [@kosamari](https://github.com/kosamari) for sharing a simple [service worker stub](https://gist.github.com/kosamari/7c5d1e8449b2fbc97d372675f16b566e).
+
 ## Licensing
 This app is licensed under GNU Affero General Public License v3.0, which will hopefully
 prevent capitalists from capitalizing this communism.
---
+++ sw.js
@@ -0,0 +1,50 @@
+var APP_PREFIX = 'CommunismButton_'
+var VERSION = '0.0.1'
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                    // Add URL you want to cache in this list.
+    '/{repository}/',           // If you have separate JS/CSS files,
+    '/{repository}/index.html'  // add path to those files here
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('Fetch request: ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Responding with cache: ' + e.request.url)
+                return request
+            } else {
+                console.log('File is not cached, fetching: ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
+// Cache resources
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Installing cache: ' + CACHE_NAME)
+            return cache.addAll(URLS)
+        })
+    )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            var cacheWhitelist = keyList.filter(key => key.indexOf(APP_PREFIX))
+            cacheWhitelist.push(CACHE_NAME)
+
+            return Promise.all(keyList.map((key, i) => {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Deleting cache: ' + keyList[i] )
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
